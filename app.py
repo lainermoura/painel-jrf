5b07cdb3c60d9c9cc31ebaed6f9ed5528d216f7b
lainermoura <56762847+lainermoura@users.noreply.github.com>
2024-04-24 17:24:49 -0300

    Adiciona Atas de Distribuição ao menu

--- app.py
+++ app.py
@@ -1,75 +1,114 @@
 import streamlit as st
 import datetime as datetime
 
-def generate_filename(tipo, sessao, turma, data, hora):
+def generate_filename(tipo, sessao, turma, data, hora, distribuicao, num_ata_distrib):
     # Formatar a data e a hora no formato desejado
     data_str = data.strftime('%Y%m%d')
     hora_str = hora.strftime('%H%M')
     ano_str = data.strftime('%Y')
 
     # Remover o "ª" da turma e garantir que seja representada com dois dígitos
-    turma = f"{int(turma.replace('ª', '')):02d}"
+    if turma is not None:
+        turma = f"{int(turma.replace('ª', '')):02d}"
 
+    extensao = '.pdf'
     # Gerar o nome do arquivo
+    #Tipo (Ata || Pauta) + Turma (00) + Sessão (000) + Ano (0000) + Data (YYYYMMDD) + Hora (HHMM) + Extensão
+    #Exemplo: ATA-JRF00-000-0000-202401010800.pdf
+
+    #ata de distribuição || Retificação da ata de distribuição
+    #1-A-ATA-DISTRIBUICAO-JRF_202401010800.pdf
+    #RETIFICACAO-1A-ATA-DISTRIBUICAO-JRF_AAAAMMDDHHMM.pdf
+
     if tipo.lower() == 'pauta':
-        filename = f"{tipo.upper()}-JRF{turma}{sessao:02d}{ano_str}_{data_str}{hora_str}"
+        filename = f"{tipo.upper()}-JRF{turma}{sessao:03d}{ano_str}_{data_str}{hora_str}{extensao}"
     elif tipo.lower() == 'ata':
-        filename = f"{tipo.upper()}-JRF{turma}{sessao:03d}{ano_str}_{data_str}{hora_str}"
+        filename = f"{tipo.upper()}-JRF{turma}{sessao:03d}{ano_str}_{data_str}{hora_str}{extensao}"
+    elif tipo.lower() == 'ata de distribuição':
+        if distribuicao == 'Ata de Distribuição':
+            filename = f"{num_ata_distrib}A-ATA-DISTRIBUICAO-JRF_{data_str}{hora_str}{extensao}"
+        elif distribuicao == 'Retificação':
+            filename = f"RETIFICACAO-{num_ata_distrib}A-ATA-DISTRIBUICAO-JRF_{data_str}{hora_str}{extensao}"
     return filename
 
+#Header|Subtitulo
+st.header(':blue[Secretaria Municipal de Fazenda - Niterói]', divider='orange')
+
+#Cria colunas para alinahar subtítulo à direita
+col1, col2 = st.columns([1,1])
+with col2:
+    st.subheader(':blue[Junta de Revisão Fiscal]')
 
 #Título
-st.title('Painel de Controle - JRF')
+st.title('Pautas, Atas e Atas de Distribuição')
 
 #Tipo, Sessão e Turma
-tipo = st.selectbox('Selecione o tipo:', ['Selecione a opção', 'Pauta', 'Ata'])
-#Turmas de 1 à 10
-turma = st.radio('Selecione a turma:', ['Selecione a opção'] + [f'{i}ª' for i in range(1, 11)]) 
-sessao = st.number_input('Digite a sessão:', 0, 1000)
+tipo = st.selectbox('Selecione o tipo:', ('Selecione a opção',  'Ata', 'Ata de Distribuição', 'Pauta'))
 
-# Verificar se o valor da sessão é 0
-if sessao == 0:
-    st.error('Digite o número da sessão.')
-elif tipo == 'Selecione a opção' or turma == 'Selecione a opção':
-    st.error('Por favor, selecione todas as opções antes de prosseguir.')
-else:
-    # Data
-    date = datetime.date.today()
-    data = st.date_input('Data da Reunião:', value=datetime.date(date.year, 1, 1), format='DD/MM/YYYY')
-# Hora
-    hora = st.time_input('Hora da Reunião:', value=datetime.time(8, 0), step=1800)
+if tipo != 'Selecione a opção':
+    if tipo == 'Ata de Distribuição':
+        distribuicao = st.radio('Selecione a opção:', ['Ata de Distribuição', 'Retificação'])
+        num_ata_distrib = st.number_input('Digite o número da ata de distribuição:', 0, 1000)
+        turma = None
+        sessao = None
+    else:
+        #Turmas de 1 à 10
+        turma = st.radio("Selecione a turma:", ['Selecione a opção'] + [f'{i}ª' for i in range(1, 11)])
+        sessao = st.number_input('Digite a sessão:', 0, 1000)
+        distribuicao = None
+        num_ata_distrib = None
 
-# Arquivo
-    uploaded_file = st.file_uploader('Selecione o arquivo:')
+    # Verificar se o valor da sessão ou num_ata_distrib é 0
+    if (sessao == 0 and tipo != 'Ata de Distribuição') or (num_ata_distrib == 0 and tipo == 'Ata de Distribuição'):
+        st.error('Digite o número da ata.')
+    elif (tipo != 'Ata de Distribuição' and turma == 'Selecione a opção') or (tipo == 'Ata de Distribuição' and distribuicao is None):
+        st.error('Por favor, selecione todas as opções antes de prosseguir.')
+    else:
+        # Data
+        date = datetime.date.today()
+        data = st.date_input('Data da Reunião:', value=date, format='DD/MM/YYYY')
 
-# Verificar se o arquivo foi carregado
-    if data == datetime.date(data.year, 1, 1):
-        st.error('Por favor, revise a data fornecida.')
-    elif hora.hour == 8 and hora.minute == 0:
-        st.error('Por favor, revise a hora fornecida.')
-    elif uploaded_file is None:
-        st.error('Por favor, carregue um arquivo.')
+        # Verificar se a data é a data atual do sistema
+        if data == date:
+            st.markdown(f'Data da Reunião: {data.strftime("%d/%m/%Y")} (**HOJE**)')
+        # Hora
+        hora = st.time_input('Hora da Reunião:', value=datetime.time(8, 0), step=1800)
 
+        # Arquivo
+        uploaded_file = st.file_uploader('Selecione o arquivo:')
 
-    # Gerar o nome do arquivo
-    if uploaded_file is not None and (hora.hour != 8 or hora.minute != 0) and data != datetime.date(data.year, 1, 1):
-        
-        # Display the selected options
-        st.write('Opções selecionadas:')
-        st.write('Tipo:', tipo)
-        st.write('Turma:', turma)
-        st.write('Sessão:', sessao)
-        st.write('Data da Reunião:', data)
-        st.write('Hora da Reunião:', hora)
+        # Verificar se o arquivo foi carregado
+        if hora.hour == 8 and hora.minute == 0:
+            st.error('Por favor, revise a hora fornecida.')
+        elif uploaded_file is None:
+            st.error('Por favor, carregue um arquivo.')
 
-        filename = generate_filename(tipo, sessao, turma, data, hora)
-        st.write('Nome do arquivo:', filename)
+        # Gerar o nome do arquivo
+        if uploaded_file is not None and (hora.hour != 8 or hora.minute != 0):
 
-        # Create columns
-        col1, col2 = st.columns([1,1])
+            # Display the selected options
+            st.write('Opções selecionadas:')
+            st.write('Tipo:', tipo)
+            if tipo != 'Ata de Distribuição':
+                st.write('Turma:', turma)
+                st.write('Sessão:', sessao)
+            else:
+                st.write('Opção:', distribuicao)
+                st.write('Número da Ata de Distribuição:', num_ata_distrib)
+            if data == date:
+                st.markdown(f'Data da Reunião: {data.strftime("%d/%m/%Y")} (**HOJE**)')
 
-        # Place the button in the right column
-        with col2:
-            if st.button('Publicar'):
-                st.success('Arquivo publicado com sucesso!')
+            else:
+                st.write('Data da Reunião:', data.strftime('%d/%m/%Y'))
+            st.write('Hora da Reunião:', hora.strftime('%H:%M'))
 
+            filename = generate_filename(tipo, sessao, turma, data, hora, distribuicao, num_ata_distrib)
+            st.write('Nome do arquivo:', filename)
+
+            # Cria colunas
+            col1, col2 = st.columns([1,1])
+
+            # Alinhamento do botão
+            with col2:
+                if st.button('Publicar'):
+                    st.success('Arquivo publicado com sucesso!')
